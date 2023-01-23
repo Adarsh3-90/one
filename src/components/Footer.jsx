9cd75984a93fa7e0327dee2a32e2fb8850de200d
Adarsh kumar <adarshkumar308855@gmail.com>
2023-01-23 15:01:21 +0530

    first commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+  <Footer>
+
+      <div>
+        <h1>TechyStar</h1>
+        <p>@all right reserved</p>
+      </div>
+
+      <div>
+        <h5>Follow Us</h5>
+        <div>
+          <a href="https://youtube.com/6packprogrammer" target={"blank"}>
+            Youtube
+          </a>
+          <a href="https://instagram.com/meabhisingh" target={"blank"}>
+            Instagram
+          </a>
+          <a href="https://github.com/meabhisingh" target={"blank"}>
+            Github
+          </a>
+        </div>
+      </div>
+  </Footer>
+  );
+  
+};
+
+export default Footer
